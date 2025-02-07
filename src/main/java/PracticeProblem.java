fd3d8973ddbd19fbb3788dfdc1f56ca15aa3592c
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-02-07 18:27:07 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -12,33 +12,60 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String input = 5 + scanner.nextLine();
+		int converted = Integer.parseInt(input) + 5;
+		System.out.println(converted);
+		scanner.close();
 	}
 
 	public static void q2() {
-		//Write question 2 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		String input = 4 + scanner.nextLine() + 3;
+		double converted = Double.parseDouble(input) + 3.4;
+		System.out.println(converted);
+		scanner.close();
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input a boolean: ");
+		String input = scanner.nextLine();
+		boolean converted = !Boolean.parseBoolean(input);
+		System.out.println(converted);
+		scanner.close();
 	}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String input = scanner.nextLine() + 3;
+		int converted = Integer.parseInt(input) + 2;
+		char secondConverted = (char) converted;
+		System.out.println(secondConverted);
+		scanner.close();
 	}
 
 	public static void q5() {
-		//Write question 5 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String input = scanner.nextLine() + 1;
+		int converted = Integer.parseInt(input) / 2;
+		double secondConverted = (double) converted;
+		System.out.println(secondConverted);
+		scanner.close();
 	}
 
 	public static void q6() {
-		//Write question 6 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		String input = 1 + scanner.nextLine();
+		double converted = Double.parseDouble(input);
+		int secondConverted = (int) converted;
+		System.out.println(secondConverted);
+		scanner.close();
 	}
 
 }
